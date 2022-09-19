48dc0c5a46edb92107292eecd0d683661c29fa30
meriem23 <esra.msallem@gmail.com>
2022-09-19 22:49:29 +0100

    finishing auth context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,32 @@
+import { useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../context/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <>
       <div className="navigation">
         <Link className="logo-conatiner" to="/">
           <CrwnLogo className="logo" />
         </Link>
-
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
-          <Link className="nav-link" to="/auth">
-            Sign In
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              Sign Out
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              Sign In
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,22 +1,24 @@
 import { initializeApp } from "firebase/app";
 import {
-    getAuth,
-    signInWithPopup,
-    signInWithRedirect,
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
+  getAuth,
+  signInWithPopup,
+  signInWithRedirect,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDbl9V3aC8bnY1Ebq_9J_HNTWv3qTkhliA",
-    authDomain: "crwn-db-12302.firebaseapp.com",
-    projectId: "crwn-db-12302",
-    storageBucket: "crwn-db-12302.appspot.com",
-    messagingSenderId: "85093954989",
-    appId: "1:85093954989:web:e600ecece607f407b05d24",
+  apiKey: "AIzaSyDbl9V3aC8bnY1Ebq_9J_HNTWv3qTkhliA",
+  authDomain: "crwn-db-12302.firebaseapp.com",
+  projectId: "crwn-db-12302",
+  storageBucket: "crwn-db-12302.appspot.com",
+  messagingSenderId: "85093954989",
+  appId: "1:85093954989:web:e600ecece607f407b05d24",
 };
 
 // Initialize Firebase
@@ -25,46 +27,46 @@ const firebaseApp = initializeApp(firebaseConfig);
 // Initialize Provider for Google auth
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
-    prompt: "select_account",
+  prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
-    signInWithPopup(auth, googleProvider);
+  signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () =>
-    signInWithRedirect(auth, googleProvider);
+  signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
-    const userDocRef = doc(db, "users", userAuth.uid);
-    // console.log(userDocRef);
-    const userSnapshot = await getDoc(userDocRef);
-    // console.log(userSnapshot);
-    // console.log(userSnapshot.exists());
-    //user data doesn't exists: create / set the document with the data from userAuth in my collection
-    if (!userSnapshot.exists()) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-        try {
-            await setDoc(userDocRef, {
-                displayName,
-                email,
-                createdAt,
-            });
-        } catch (error) {
-            console.log("error creating the user", error);
-        }
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  //user data doesn't exists: create / set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error);
     }
-    //if user data exists
-    return userDocRef;
+  }
+  //if user data exists
+  return userDocRef;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
-    return await createUserWithEmailAndPassword(auth, email, password);
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password);
 };
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-    if (!email || !password) return;
-    return await signInWithEmailAndPassword(auth, email, password);
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password);
 };
+export const signOutUser = async () => await signOut(auth);
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback)
