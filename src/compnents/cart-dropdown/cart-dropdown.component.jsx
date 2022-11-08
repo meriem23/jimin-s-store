54a3f70c36944b654edb19d2509a08bd4c6192a6
meriem23 <esra.msallem@gmail.com>
2022-11-08 16:42:26 +0000

    adding products to the cart product

--- src/compnents/cart-dropdown/cart-dropdown.component.jsx
+++ src/compnents/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,18 @@
-import "./cart-dropdown.styles.scss";
+import {useContext} from "react"
+//context
+import {CartContext} from "../../context/cart.context"
+//components
 import Button from "../button/button.component";
-
+import CartItem from "../cart-item/cart-item.component";
+//styles
+import "./cart-dropdown.styles.scss";
 const CartDropdown = () => {
+  const {cartItems}= useContext(CartContext)
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+     {   cartItems.map(item => <CartItem cartItem={item} key={item.id}/>)}
+      </div>
       <Button>Checkout</Button>
     </div>
   );
