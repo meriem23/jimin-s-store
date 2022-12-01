b35dfec276e7be29c01da0c1507c84cbd1add615
meriem23 <esra.msallem@gmail.com>
2022-12-01 16:38:55 +0100

    created checkout page, checkout items

--- src/compnents/cart-icon/cart-icon.component.jsx
+++ src/compnents/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,18 @@
-import {useContext} from "react"
+import { useContext } from "react";
 //assets
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 //context
-import {CartContext} from "../../context/cart.context"
+import { CartContext } from "../../context/cart.context";
 //styles
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const {isCartOpen,setIsCartOpen}= useContext(CartContext)
-  const toggleCartOpen = ()=> setIsCartOpen(!isCartOpen)
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+  const toggleCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
     <div className="cart-icon-container" onClick={toggleCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">10</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
